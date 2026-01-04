ab365d078697849d7e826b7beb7937b6f700d7b9
Rasbats <miker@members.swis.net>
2026-01-04 12:10:18 +0000

    Try to get back

--- src/rower_pi.cpp
+++ src/rower_pi.cpp
@@ -1,11 +1,11 @@
 /******************************************************************************
  *
  * Project:  OpenCPN
- * Purpose:  Timer Plugin
+ * Purpose: rower Plugin
  * Author:   Mike Rossiter
  *
  ***************************************************************************
- *   Copyright (C) 2013 by Mike Rossiter                                *
+ *   Copyright (C) 2017 by Mike Rossiter                                *
  *   $EMAIL$                                                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -29,27 +29,33 @@
 
 #ifndef WX_PRECOMP
 #include "wx/wx.h"
-#endif  // precompiled headers
+#endif
 
+#include "config.h"
+#include "icons.h"
 #include "rower_pi.h"
-#include "rowergui_impl.h"
-#include "rowergui.h"
+#include "rower_gui.h"
+#include "rower_gui_impl.h"
+#include "ocpn_plugin.h"
+#include "plug_utils.h"
+
+class Dlg;
+
+using namespace std;
 
 // the class factories, used to create and destroy instances of the PlugIn
 
-extern "C" DECL_EXP opencpn_plugin *create_pi(void *ppimgr) {
-  return new e_timer_pi(ppimgr);
+extern "C" DECL_EXP opencpn_plugin* create_pi(void* ppimgr) {
+  return new rowerPi(ppimgr);
 }
 
-extern "C" DECL_EXP void destroy_pi(opencpn_plugin *p) { delete p; }
+extern "C" DECL_EXP void destroy_pi(opencpn_plugin* p) { delete p; }
 
 //---------------------------------------------------------------------------------------------------------
 //
-//    Timer PlugIn Implementation
+//   rower PlugIn Implementation
 //
 //---------------------------------------------------------------------------------------------------------
-
-#include "icons.h"
 
 //---------------------------------------------------------------------------------------------------------
 //
@@ -57,87 +63,135 @@ extern "C" DECL_EXP void destroy_pi(opencpn_plugin *p) { delete p; }
 //
 //---------------------------------------------------------------------------------------------------------
 
-e_timer_pi::e_timer_pi(void *ppimgr) : opencpn_plugin_118(ppimgr) {
+rowerPi::rowerPi(void* ppimgr)
+    : opencpn_plugin_118(ppimgr),
+      m_hr_dialog_x(0),
+      m_hr_dialog_y(8),
+      m_tr_spd(0),
+      m_tr_dir(0),
+      m_cursor_lat(0),
+      m_cursor_lon(0),
+      m_position_menu_id(-1),
+      m_gui_scale_factor(1),
+      plugin(nullptr),
+      m_dialog(nullptr),
+      m_config(nullptr),
+      m_parent_window(nullptr),
+      m_hr_dialog_width(0),
+      m_hr_dialog_height(0),
+      m_hr_dialog_sx(0),
+      m_hr_dialog_sy(0),
+      m_display_width(0),
+      m_display_height(0),
+      m_leftclick_tool_id(-1),
+      m_show_rower_icon(false),
+      m_copy_use_ais(false),
+      m_copy_use_file(false), 
+      m_copy_use_nmea(false) 
+
+{
   // Create the PlugIn icons
   initialize_images();
-
-  wxFileName fn;
-  wxString tmp_path;
-
-  tmp_path = GetPluginDataDir("e_timer_pi");
-  fn.SetPath(tmp_path);
-  fn.AppendDir("data");
-  fn.SetFullName("e_timer_pi_panel_icon.png");
-
-  wxString shareLocn = fn.GetFullPath();
-  wxImage panelIcon(shareLocn);
-
-  if (panelIcon.IsOk())
-    m_panelBitmap = wxBitmap(panelIcon);
+  auto icon_path = GetPluginIcon("rower_panel_icon", PKG_NAME);
+  if (icon_path.type == IconPath::Type::Svg)
+    m_panel_bitmap = LoadSvgIcon(icon_path.path.c_str());
+  else if (icon_path.type == IconPath::Type::Png)
+    m_panel_bitmap = LoadPngIcon(icon_path.path.c_str());
+  else  // icon_path.type == NotFound
+    wxLogWarning("Cannot find icon for basename: %s", "rower_panel_icon");
+  if (m_panel_bitmap.IsOk())
+    wxLogDebug("rowerPi::, bitmap OK");
   else
-    wxLogMessage("e_timer_pi panel icon NOT loaded");
-  m_bShowTimer = false;
+    wxLogDebug("rowerPi::, bitmap fail");
+  m_show_rower = false;
 }
 
-e_timer_pi::~e_timer_pi(void) {
-  delete _img_e_timer_pi;
-  delete _img_Timer;
+rowerPi::~rowerPi() {
+  delete _img_rowerIcon;
+
+  if (m_dialog) {
+    wxFileConfig* pConf = GetOCPNConfigObject();
+
+    if (pConf) {
+      pConf->SetPath("/PlugIns/rower_pi");
+      pConf->Write("rowerUseAis", m_copy_use_ais);
+      pConf->Write("rowerUseFile", m_copy_use_file);
+      pConf->Write("rowerMMSI", m_copy_mmsi);
+      pConf->Write("rowerUseNMEA", m_copy_use_nmea);
+    }
+  }
 }
 
-int e_timer_pi::Init(void) {
-  AddLocaleCatalog("opencpn-e_timer_pi");
+int rowerPi::Init() {
+  AddLocaleCatalog("opencpn-rower_pi");
 
   // Set some default private member parameters
-  m_route_dialog_x = 0;
-  m_route_dialog_y = 0;
+  m_hr_dialog_x = 40;
+  m_hr_dialog_y = 80;
+  m_hr_dialog_sx = 400;
+  m_hr_dialog_sy = 300;
   ::wxDisplaySize(&m_display_width, &m_display_height);
 
-  //    Get a pointer to the opencpn display canvas, to use as a parent for the
-  //    POI Manager dialog
+  //    Get a pointer to the opencpn display canvas, to use as a parent for
+  //    the POI Manager dialog
   m_parent_window = GetOCPNCanvasWindow();
 
   //    Get a pointer to the opencpn configuration object
-  m_pconfig = GetOCPNConfigObject();
+  m_config = GetOCPNConfigObject();
 
   //    And load the configuration items
   LoadConfig();
-
+  auto icon = GetPluginIcon("rower_pi", PKG_NAME);
+  auto toggled_icon = GetPluginIcon("rower_pi_toggled", PKG_NAME);
   //    This PlugIn needs a toolbar icon, so request its insertion
-  if (m_bTimerShowIcon)
-
-#ifdef ocpnUSE_SVG
-    m_leftclick_tool_id = InsertPlugInToolSVG(
-        "Timer", _svg_timer, _svg_timer, _svg_timer_toggled, wxITEM_CHECK,
-        "Timer", "", NULL, CALCULATOR_TOOL_POSITION, 0, this);
-#else
-    m_leftclick_tool_id = InsertPlugInTool(
-        "", _img_Timer, _img_Timer, wxITEM_CHECK, "Timer"), "", NULL,
-        CALCULATOR_TOOL_POSITION, 0, this);
-#endif
-
-  m_pDialog = NULL;
+  if (m_show_rower_icon) {
+    if (icon.type == IconPath::Type::Svg)
+      m_leftclick_tool_id = InsertPlugInToolSVG(
+          "rower", icon.path, icon.path, toggled_icon.path, wxITEM_CHECK,
+          "rower", "", nullptr,rower_TOOL_POSITION, 0, this);
+    else if (icon.type == IconPath::Type::Png) {
+      auto bitmap = LoadPngIcon(icon.path.c_str());
+      m_leftclick_tool_id =
+          InsertPlugInTool("", &bitmap, &bitmap, wxITEM_CHECK, "rower", "",
+                           nullptr,rower_TOOL_POSITION, 0, this);
+    }
+  }
+  m_dialog = nullptr;
 
   return (WANTS_OVERLAY_CALLBACK | WANTS_OPENGL_OVERLAY_CALLBACK |
-
-          WANTS_CURSOR_LATLON | WANTS_TOOLBAR_CALLBACK | INSTALLS_TOOLBAR_TOOL |
-          WANTS_CONFIG | WANTS_PLUGIN_MESSAGING
-
-  );
+          WANTS_TOOLBAR_CALLBACK | INSTALLS_TOOLBAR_TOOL | WANTS_CURSOR_LATLON |
+          WANTS_NMEA_SENTENCES | WANTS_AIS_SENTENCES | WANTS_PREFERENCES |
+          WANTS_PLUGIN_MESSAGING | WANTS_CONFIG);
 }
 
-bool e_timer_pi::DeInit(void) {
+bool rowerPi::DeInit() {
   //    Record the dialog position
-  if (NULL != m_pDialog) {
+  if (m_dialog) {
     // Capture dialog position
-    wxPoint p = m_pDialog->GetPosition();
-    SetCalculatorDialogX(p.x);
-    SetCalculatorDialogY(p.y);
-    m_pDialog->Close();
-    delete m_pDialog;
-    m_pDialog = NULL;
+    wxPoint p = m_dialog->GetPosition();
+    wxRect r = m_dialog->GetRect();
+    SetrowerDialogX(p.x);
+    SetrowerDialogY(p.y);
+    SetrowerDialogSizeX(r.GetWidth());
+    SetrowerDialogSizeY(r.GetHeight());
+    if (m_copy_use_nmea) {
+      if (m_dialog->nmeastream->IsOpened()) {
+        m_dialog->nmeastream->Write();
+        m_dialog->nmeastream->Close();
+      }
+    }
+  
 
-    m_bShowTimer = false;
-    SetToolbarItemState(m_leftclick_tool_id, m_bShowTimer);
+    if ((m_dialog->m_timer) && (m_dialog->m_timer->IsRunning())) {
+      // need to stop the timer or crash on exit
+      m_dialog->m_timer->Stop();
+    }
+    m_dialog->Close();
+    delete m_dialog;
+    m_dialog = nullptr;
+
+    m_show_rower = false;
+    SetToolbarItemState(m_leftclick_tool_id, m_show_rower);
   }
 
   SaveConfig();
@@ -147,102 +201,231 @@ bool e_timer_pi::DeInit(void) {
   return true;
 }
 
-int e_timer_pi::GetAPIVersionMajor() { return atoi(API_VERSION); }
+int rowerPi::GetAPIVersionMajor() { return atoi(API_VERSION); }
 
-int e_timer_pi::GetAPIVersionMinor() {
+int rowerPi::GetAPIVersionMinor() {
   std::string v(API_VERSION);
   size_t dotpos = v.find('.');
   return atoi(v.substr(dotpos + 1).c_str());
 }
 
-int e_timer_pi::GetPlugInVersionMajor() { return PLUGIN_VERSION_MAJOR; }
+int rowerPi::GetPlugInVersionMajor() { return PLUGIN_VERSION_MAJOR; }
 
-int e_timer_pi::GetPlugInVersionMinor() { return PLUGIN_VERSION_MINOR; }
+int rowerPi::GetPlugInVersionMinor() { return PLUGIN_VERSION_MINOR; }
 
-int GetPlugInVersionPatch() { return PLUGIN_VERSION_PATCH; }
+int rowerPi::GetPlugInVersionPatch() { return PLUGIN_VERSION_PATCH; }
 
-int GetPlugInVersionPost() { return PLUGIN_VERSION_TWEAK; }
+int rowerPi::GetPlugInVersionPost() { return PLUGIN_VERSION_TWEAK; };
 
-const char *GetPlugInVersionPre() { return PKG_PRERELEASE; }
+const char*rowerPi::GetPlugInVersionPre() { return PKG_PRERELEASE; }
 
-const char *GetPlugInVersionBuild() { return PKG_BUILD_INFO; }
+const char*rowerPi::GetPlugInVersionBuild() { return PKG_BUILD_INFO; }
 
-wxBitmap *e_timer_pi::GetPlugInBitmap() { return &m_panelBitmap; }
+wxBitmap*rowerPi::GetPlugInBitmap() { return &m_panel_bitmap; }
 
-wxString e_timer_pi::GetCommonName() { return PLUGIN_API_NAME; }
-wxString e_timer_pi::GetShortDescription() { return PKG_SUMMARY; }
-wxString e_timer_pi::GetLongDescription() { return PKG_DESCRIPTION; }
+wxString rowerPi::GetCommonName() { return PLUGIN_API_NAME; }
 
-int e_timer_pi::GetToolbarToolCount(void) { return 1; }
+wxString rowerPi::GetShortDescription() { return PKG_SUMMARY; }
 
-void e_timer_pi::SetColorScheme(PI_ColorScheme cs) {
-  if (NULL == m_pDialog) return;
+wxString rowerPi::GetLongDescription() { return PKG_DESCRIPTION; }
 
-  DimeWindow(m_pDialog);
+int rowerPi::GetToolbarToolCount() { return 1; }
+
+void rowerPi::SetColorScheme(PI_ColorScheme cs) {
+  if (!m_dialog) return;
+  DimeWindow(m_dialog);
 }
 
-void e_timer_pi::OnToolbarToolCallback(int id) {
-  if (NULL == m_pDialog) {
-    m_pDialog = new Dlg(m_parent_window, this);
-    m_pDialog->Move(wxPoint(m_route_dialog_x, m_route_dialog_y));
+void rowerPi::ShowPreferencesDialog(wxWindow* parent) {
+  auto* pref = new rowerPreferences(parent);
+
+  pref->m_cbTransmitAis->SetValue(m_copy_use_ais);
+  pref->m_cbAisToFile->SetValue(m_copy_use_file);
+  pref->m_textCtrlMMSI->SetValue(m_copy_mmsi);
+  pref->m_cbNMEAToFile->SetValue(m_copy_use_nmea);
+
+  if (pref->ShowModal() == wxID_OK) {
+    bool copy_ais = pref->m_cbTransmitAis->GetValue();
+    bool copy_file = pref->m_cbAisToFile->GetValue();
+    wxString copyMMSI = pref->m_textCtrlMMSI->GetValue();
+    bool copy_nmea = pref->m_cbNMEAToFile->GetValue();
+
+
+    if (m_copy_use_ais != copy_ais || m_copy_use_file != copy_file ||
+        m_copy_mmsi != copyMMSI) {
+      m_copy_use_ais = copy_ais;
+      m_copy_use_file = copy_file;
+      m_copy_mmsi = copyMMSI;
+    }
+
+    if (m_copy_use_nmea != copy_nmea) {
+      m_copy_use_nmea = copy_nmea;
+    }
+
+    if (m_dialog) {
+      m_dialog->m_bUseAis = m_copy_use_ais;
+      m_dialog->m_bUseFile = m_copy_use_file;
+      m_dialog->m_tMMSI = m_copy_mmsi;
+      m_dialog->m_bUseNMEA = m_copy_use_nmea;
+    }
+
+    SaveConfig();
+
+    RequestRefresh(m_parent_window);  // refresh main window
   }
 
-  m_pDialog->Fit();
+  delete pref;
+  pref = nullptr;
+}
+
+void rowerPi::OnToolbarToolCallback(int id) {
+  if (!m_dialog) {
+    m_dialog = new Dlg(m_parent_window);
+    m_dialog->plugin = this;
+    m_dialog->m_timer = new wxTimer(m_dialog);
+    m_dialog->Move(wxPoint(m_hr_dialog_x, m_hr_dialog_y));
+    m_dialog->SetSize(m_hr_dialog_sx, m_hr_dialog_sy);
+
+    wxMenu dummy_menu;
+    m_position_menu_id = AddCanvasContextMenuItem(
+        new wxMenuItem(&dummy_menu, -1, _("Select Vessel Start Position")),
+        this);
+    SetCanvasContextMenuItemViz(m_position_menu_id, true);
+  }
+
+  // m_pDialog->Fit();
   // Toggle
-  m_bShowTimer = !m_bShowTimer;
+  m_show_rower = !m_show_rower;
 
   //    Toggle dialog?
-  if (m_bShowTimer) {
-    m_pDialog->Show();
-  } else
-    m_pDialog->Hide();
+  if (m_show_rower) {
+    m_dialog->Move(wxPoint(m_hr_dialog_x, m_hr_dialog_y));
+    m_dialog->SetSize(m_hr_dialog_sx, m_hr_dialog_sy);
+    m_dialog->Show();
+
+  } else {
+    m_dialog->Hide();
+  }
 
   // Toggle is handled by the toolbar but we must keep plugin manager b_toggle
   // updated to actual status to ensure correct status upon toolbar rebuild
-  SetToolbarItemState(m_leftclick_tool_id, m_bShowTimer);
+  SetToolbarItemState(m_leftclick_tool_id, m_show_rower);
+
+  // Capture dialog position
+  wxPoint p = m_dialog->GetPosition();
+  wxRect r = m_dialog->GetRect();
+  SetrowerDialogX(p.x);
+  SetrowerDialogY(p.y);
+  SetrowerDialogSizeX(r.GetWidth());
+  SetrowerDialogSizeY(r.GetHeight());
 
   RequestRefresh(m_parent_window);  // refresh main window
 }
 
-bool e_timer_pi::LoadConfig(void) {
-  wxFileConfig *pConf = (wxFileConfig *)m_pconfig;
+bool rowerPi::LoadConfig() {
+  auto* conf = (wxFileConfig*)m_config;
 
-  if (pConf) {
-    pConf->SetPath("/Settings/e_timer_pi");
-    pConf->Read("ShowTimerIcon", &m_bTimerShowIcon, 1);
+  if (conf) {
+    if (conf->HasGroup(_T("/Settings/rower_pi"))) {
+      // Read the existing settings
 
-    m_route_dialog_x = pConf->Read("DialogPosX", 20L);
-    m_route_dialog_y = pConf->Read("DialogPosY", 20L);
+      conf->SetPath("/Settings/rower_pi");
+      conf->Read("ShowrowerIcon", &m_show_rower_icon, true);
+      conf->Read("rowerUseAis", &m_copy_use_ais, false);
+      conf->Read("rowerUseNMEA", &m_copy_use_nmea, false);
+      conf->Read("rowerUseFile", &m_copy_use_file, false);
+      m_copy_mmsi = conf->Read("rowerMMSI", "123456789");
 
-    if ((m_route_dialog_x < 0) || (m_route_dialog_x > m_display_width))
-      m_route_dialog_x = 5;
-    if ((m_route_dialog_y < 0) || (m_route_dialog_y > m_display_height))
-      m_route_dialog_y = 5;
+      m_hr_dialog_x = conf->Read("DialogPosX", 40L);
+      m_hr_dialog_y = conf->Read("DialogPosY", 140L);
+      m_hr_dialog_sx = conf->Read("DialogSizeX", 330L);
+#ifdef __WXOSX__
+      m_hr_dialog_sy = conf->Read("DialogSizeY", 250L);
+#else
+      m_hr_dialog_sy = conf->Read("DialogSizeY", 300L);
+#endif
+      conf->DeleteGroup(_T("/Settings/rower_pi"));
+    } else {
+      conf->SetPath("/PlugIns/rower_pi");
+      conf->Read("ShowrowerIcon", &m_show_rower_icon, true);
+      conf->Read("rowerUseAis", &m_copy_use_ais, false);
+      conf->Read("rowerUseNMEA", &m_copy_use_nmea, false);
+      conf->Read("rowerUseFile", &m_copy_use_file, false);
+      m_copy_mmsi = conf->Read("rowerMMSI", "123456789");
+
+      m_hr_dialog_x = conf->Read("DialogPosX", 40L);
+      m_hr_dialog_y = conf->Read("DialogPosY", 140L);
+      m_hr_dialog_sx = conf->Read("DialogSizeX", 330L);
+#ifdef __WXOSX__
+      m_hr_dialog_sy = conf->Read("DialogSizeY", 250L);
+#else
+      m_hr_dialog_sy = conf->Read("DialogSizeY", 300L);
+#endif
+    }
+    if ((m_hr_dialog_x < 0) || (m_hr_dialog_x > m_display_width))
+      m_hr_dialog_x = 40;
+    if ((m_hr_dialog_y < 0) || (m_hr_dialog_y > m_display_height))
+      m_hr_dialog_y = 140;
+
     return true;
   } else
     return false;
 }
 
-bool e_timer_pi::SaveConfig(void) {
-  wxFileConfig *pConf = (wxFileConfig *)m_pconfig;
+bool rowerPi::SaveConfig() {
+  auto* conf = (wxFileConfig*)m_config;
 
-  if (pConf) {
-    pConf->SetPath("/Settings/e_timer_pi");
-    pConf->Write("ShowTimerIcon", m_bTimerShowIcon);
+  if (conf) {
+    bool is_digits = m_copy_mmsi.IsNumber();
+    if (m_copy_mmsi.length() < 9 || !is_digits) {
+      wxMessageBox(_("MMSI must be 9 digits.\nEdit using Preferences"));
+      return false;
+    }
 
-    pConf->Write("DialogPosX", m_route_dialog_x);
-    pConf->Write("DialogPosY", m_route_dialog_y);
+    conf->SetPath("/PlugIns/rower_pi");
+    conf->Write("ShowrowerIcon", m_show_rower_icon);
+    conf->Write("rowerUseAis", m_copy_use_ais);
+    conf->Write("rowerUseNMEA", m_copy_use_nmea);
+    conf->Write("rowerUseFile", m_copy_use_file);
+    conf->Write("rowerMMSI", m_copy_mmsi);
+
+    conf->Write("DialogPosX", m_hr_dialog_x);
+    conf->Write("DialogPosY", m_hr_dialog_y);
+    conf->Write("DialogSizeX", m_hr_dialog_sx);
+    conf->Write("DialogSizeY", m_hr_dialog_sy);
 
     return true;
   } else
     return false;
 }
 
-void e_timer_pi::OnTimerDialogClose() {
-  m_bShowTimer = false;
-  SetToolbarItemState(m_leftclick_tool_id, m_bShowTimer);
-  m_pDialog->Hide();
+void rowerPi::OnrowerDialogClose() {
+  m_show_rower = false;
+  SetToolbarItemState(m_leftclick_tool_id, m_show_rower);
+  m_dialog->Hide();
   SaveConfig();
 
   RequestRefresh(m_parent_window);  // refresh main window
+}
+
+void rowerPi::OnContextMenuItemCallback(int id) {
+  if (!m_dialog) return;
+
+  if (id == m_position_menu_id) {
+    m_cursor_lat = GetCursorLat();
+    m_cursor_lon = GetCursorLon();
+
+    m_dialog->OnContextMenu(m_cursor_lat, m_cursor_lon);
+  }
+}
+
+void rowerPi::SetCursorLatLon(double lat, double lon) {
+  m_cursor_lat = lat;
+  m_cursor_lon = lon;
+}
+
+void rowerPi::SetNMEASentence(wxString& sentence) {
+  if (m_dialog) {
+    m_dialog->parseNMEASentence(sentence);
+  }
 }
--- src/rower_pi.h
+++ src/rower_pi.h
@@ -1,11 +1,11 @@
 /******************************************************************************
  *
  * Project:  OpenCPN
- * Purpose:  Timer Plugin
+ * Purpose: rower Plugin
  * Author:   Mike Rossiter
  *
  ***************************************************************************
- *   Copyright (C) 2013 by Mike Rossiter                                   *
+ *   Copyright (C) 2017 by Mike Rossiter                                   *
  *   $EMAIL$                                                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -25,89 +25,118 @@
  ***************************************************************************
  */
 
-#ifndef _CALCULATOR_PI_H_
-#define _CALCULATOR_PI_H_
+#ifndef SDR_PI_H_
+#define SDR_PI_H_
 
 #include "wx/wxprec.h"
 
-#ifndef  WX_PRECOMP
-  #include "wx/wx.h"
-  #include <wx/glcanvas.h>
-#endif //precompiled headers
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#include <wx/glcanvas.h>
+#endif
 
 #include <wx/fileconf.h>
-
-#include "ocpn_plugin.h" //Required for OCPN plugin functions
-#include "rowergui_impl.h"
-#include "rowergui.h"
+#include <wx/datetime.h>
+#include <wx/tokenzr.h>
 
 #include "config.h"
 
+
+
+#include "ocpn_plugin.h"  //Required for OCPN plugin functions
+#include "rower_gui_impl.h"
+
+
+class Dlg;
 
 //----------------------------------------------------------------------------------------------------------
 //    The PlugIn Class Definition
 //----------------------------------------------------------------------------------------------------------
 
-#define CALCULATOR_TOOL_POSITION    -1          // Request default positioning of toolbar tool
+#define rower_TOOL_POSITION (-1)  ///< toolbar tool default positioning
 
-class Dlg;
-
-class e_timer_pi : public opencpn_plugin_118
-{
+class rowerPi : public opencpn_plugin_118 {
 public:
-      e_timer_pi(void *ppimgr);
-	   ~e_timer_pi(void);
+  explicit rowerPi(void* ppimgr);
+  ~rowerPi() override;
 
-//    The required PlugIn Methods
-      int Init(void);
-      bool DeInit(void);
+  //    The required PlugIn Methods
+  int Init() override;
+  bool DeInit() override;
 
-      int GetAPIVersionMajor();
-      int GetAPIVersionMinor();
-      int GetPlugInVersionMajor();
-      int GetPlugInVersionMinor();
-      wxBitmap *GetPlugInBitmap();
-      wxString GetCommonName();
-      wxString GetShortDescription();
-      wxString GetLongDescription();
+  int GetAPIVersionMajor() override;
+  int GetAPIVersionMinor() override;
+  int GetPlugInVersionMajor() override;
+  int GetPlugInVersionMinor() override;
+  int GetPlugInVersionPatch() override;
+  int GetPlugInVersionPost() override;
+  const char* GetPlugInVersionPre() override;
+  const char* GetPlugInVersionBuild() override;
 
-//    The required override PlugIn Methods
-      int GetToolbarToolCount(void);
-      void OnToolbarToolCallback(int id);
-     
+  wxBitmap* GetPlugInBitmap() override;
+  wxString GetCommonName() override;
+  wxString GetShortDescription() override;
+  wxString GetLongDescription() override;
 
-//    Optional plugin overrides
-      void SetColorScheme(PI_ColorScheme cs);
+  //    The required override PlugIn Methods
+  int GetToolbarToolCount() override;
+  void OnToolbarToolCallback(int id) override;
 
+  //    Optional plugin overrides
+  void SetColorScheme(PI_ColorScheme cs) override;
 
-//    The override PlugIn Methods
+  //    The override PlugIn Methods
+  void OnContextMenuItemCallback(int id) override;
+  void SetCursorLatLon(double lat, double lon) override;
+  void SetNMEASentence(wxString& sentence) override;
 
-//    Other public methods
-      void SetCalculatorDialogX         (int x){ m_route_dialog_x = x;};
-      void SetCalculatorDialogY         (int x){ m_route_dialog_y = x;};
-      void SetCalculatorDialogWidth     (int x){ m_route_dialog_width = x;};
-      void SetCalculatorDialogHeight    (int x){ m_route_dialog_height = x;};      
-	  void OnTimerDialogClose();
+  //    Other public methods
+  void SetrowerDialogX(int x) { m_hr_dialog_x = x; };
+  void SetrowerDialogY(int x) { m_hr_dialog_y = x; };
+  void SetrowerDialogWidth(int x) { m_hr_dialog_width = x; };
+  void SetrowerDialogHeight(int x) { m_hr_dialog_height = x; };
+  void SetrowerDialogSizeX(int x) { m_hr_dialog_sx = x; }
+  void SetrowerDialogSizeY(int x) { m_hr_dialog_sy = x; }
+  void OnrowerDialogClose();
 
-	  wxWindow         *m_parent_window;
-	  
+  int m_hr_dialog_x, m_hr_dialog_y;
+
+  [[nodiscard]] double GetCursorLat() const { return m_cursor_lat; }
+  [[nodiscard]] double GetCursorLon() const { return m_cursor_lon; }
+
+  void ShowPreferencesDialog(wxWindow* parent) override;
+
+  double m_tr_spd;
+  double m_tr_dir;
+  wxBitmap m_panel_bitmap;
+
 private:
-      
-	  Dlg               *m_pDialog;
-	  wxFileConfig      *m_pconfig;
-     
-      bool              LoadConfig(void);
-      bool              SaveConfig(void);
-      
-      int               m_route_dialog_x, m_route_dialog_y,m_route_dialog_width,m_route_dialog_height;
-      int               m_display_width, m_display_height;      
-      int               m_leftclick_tool_id;
-      bool              m_ShowHelp,m_bCaptureCursor,m_bCaptureShip;
-      double m_ship_lon,m_ship_lat,m_cursor_lon,m_cursor_lat;
+  double m_cursor_lat;
+  double m_cursor_lon;
 
-	  bool             m_bTimerShowIcon;
-	  bool             m_bShowTimer;
-	  wxBitmap			m_panelBitmap;
+  int m_position_menu_id;
+  double m_gui_scale_factor;
+
+ rowerPi* plugin;
+
+  Dlg* m_dialog;
+
+  wxFileConfig* m_config;
+  wxWindow* m_parent_window;
+  bool LoadConfig();
+  bool SaveConfig();
+
+  int m_hr_dialog_width, m_hr_dialog_height;
+  int m_hr_dialog_sx, m_hr_dialog_sy;
+  int m_display_width, m_display_height;
+  int m_leftclick_tool_id;
+  bool m_show_rower_icon;
+  bool m_show_rower;
+
+  bool m_copy_use_ais;
+  bool m_copy_use_file;
+  wxString m_copy_mmsi;
+  bool m_copy_use_nmea;
 };
 
 #endif
